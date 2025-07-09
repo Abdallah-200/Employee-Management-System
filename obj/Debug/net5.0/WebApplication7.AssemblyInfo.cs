18b98f9afa1d75898ff993e23e153db2ff5e5b2b
Your Name <you@example.com>
2025-07-09 18:01:34 +0300

    Renamed screenshots

--- obj/Debug/net5.0/WebApplication7.AssemblyInfo.cs
+++ obj/Debug/net5.0/WebApplication7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f55cd28b2501b86e298c9cb96c8570803124953c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
